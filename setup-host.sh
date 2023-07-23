f67c50706fc22b7bf090d1ab41904a9d26c29c89
brederle <rbe-github@t-online.de>
2023-07-23 14:25:19 +0200

    Init homeassistant container setup with secrets and env

--- setup-host.sh
+++ setup-host.sh
@@ -2,8 +2,7 @@
 
 # MQTT Mosquitto user
 adduser --system --uid 1883 --group --disabled-login --shell /sbin/nologin mosquitto
-usermod mosquitto --comment "NATHAN mosquitto user" mosquitto
-sudo -u mosquitto mkdir -p /home/mosquitto/.nathan/.sec
+usermod --comment "NATHAN mosquitto user" mosquitto
 sudo -u mosquitto mkdir -p /home/mosquitto/.nathan/config
 sudo -u mosquitto mkdir -p /home/mosquitto/.nathan/log
 sudo -u mosquitto mkdir -p /home/mosquitto/.nathan/data
@@ -12,3 +11,7 @@ sudo -u mosquitto mkdir -p /home/mosquitto/.nathan/data
 docker run -ti -u 1883 -v /home/mosquitto/.nathan/.sec:/mosquitto/.sec \
   eclipse-mosquitto:latest mosquitto_passwd -c /mosquitto/.sec/mosquitto.passwd homeassist
 sudo -u mosquitto docker rm <hash>
+
+# Homesistant user
+adduser --system --uid 1884 --group --disabled-login --shell /sbin/nologin assistant
+usermod --comment "NATHAN home assistant user" assistant
