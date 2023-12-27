3e04fe9de5af090c848c762ee2ef92e10d0baa05
brederle <rbe-github@t-online.de>
2023-12-27 13:15:58 +0100

    Simplify docker-compose shell wrapper

--- compose.sh
+++ compose.sh
@@ -4,5 +4,6 @@ NATHAN_MQTT_VERSION=2.0-2 \
 NATHAN_HASS_VERSION=2023.12-1 \
 NATHAN_ZWAVEJS_VERSION=9.6-1 \
 NATHAN_VERSION=23.0.2 \
-NATHAN_USER=digital \
-docker-compose up
+NATHAN_GROUP=$(getent group homeauto  | cut -d: -f3) \
+DIALOUT_GROUP=$(getent group dialout | cut -d: -f3) \
+docker-compose $@
