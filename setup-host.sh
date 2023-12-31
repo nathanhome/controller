cd23bba9a3be094ff4fbf3f12125b498507dc35b
brederle <rbe-github@t-online.de>
2023-12-31 11:02:06 +0100

    Debug hass configuration

--- setup-host.sh
+++ setup-host.sh
@@ -2,39 +2,50 @@
 
 addgroup --gid 1882 homeauto
 
-sudo -u digital mkdir --mode 770 -p /home/digital/.nathan/logs
-sudo -u digital chown digital:homeauto /home/digital/.nathan/logs
-
 # MQTT Mosquitto user
 adduser --system --uid 1883 --group --disabled-login --shell /sbin/nologin mosquitto
 usermod --comment "NATHAN mosquitto user" mosquitto
-sudo -u mosquitto mkdir -p /home/mosquitto/.nathan/config
-sudo -u mosquitto mkdir -p /home/mosquitto/.nathan/log
-sudo -u mosquitto mkdir -p /home/mosquitto/.nathan/data
+mkdir -p /home/mosquitto/.nathan/config
+mkdir -p /home/mosquitto/.nathan/data
 usermod -a -G homeauto mosquitto
+
+mkdir -p /var/log/mqbroker
+chown mosquitto:mosquitto /var/log/mqbroker
+chmod 750 /var/log/mqbroker
 
 # create mosquitto passwd file
 docker run -ti -u 1883 -v /home/mosquitto/.nathan/.sec:/mosquitto/.sec \
   eclipse-mosquitto:latest mosquitto_passwd -c /mosquitto/.sec/mosquitto.passwd assistant
-sudo -u mosquitto docker rm <hash>
+# sudo -u mosquitto docker rm <hash>
 
 # Homeassistant user
 adduser --system --uid 1884 --group --disabled-login --shell /sbin/nologin assistant
 usermod --comment "NATHAN home assistant user" assistant
 usermod -a -G homeauto assistant
 
+mkdir -p /var/log/homeassistant
+chown assistant:homeauto /var/log/homeassistant
+chmod 750 /var/log/homeassistant
+
 # Zwave gateway user
 adduser --system --uid 1885 --group --disabled-login --shell /sbin/nologin zwgate
 usermod --comment "NATHAN zwavejs2mqtt user" zwgate
-sudo -u zwgate mkdir -p /home/zwgate/.nathan/data
-sudo -u zwgate mkdir -p /mnt/nathan/zwgate/backup
+mkdir -p /home/zwgate/.nathan/data
+mkdir -p /mnt/nathan/zwgate/backup
 usermod -a -G homeauto,dialout zwgate
+
+mkdir -p /var/log/zwgate
+chown zwgate:homeauto /var/log/zwgate
+chmod 750 /var/log/zwgate
 
 # Zigbee gateway user
 adduser --system --uid 1886 --group --disabled-login --shell /sbin/nologin zigate
 usermod --comment "NATHAN zigbee2mqtt user" zigate
 sudo -u zigate mkdir -p /home/zigate/.nathan/data
 #sudo -u zigate mkdir -p /mnt/nathan/zwgate/backup
-usermod -a -G homeauto
+usermod -a -G homeauto,dialout zigate
 
+mkdir -p /var/log/zigate
+chown zigate:homeauto /var/log/zigate
+chmod 750 /var/log/zigate
 
