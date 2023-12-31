ad41ce2a7126651e5a5647fc030266e7a3679fc5
brederle <rbe-github@t-online.de>
2023-12-31 11:01:33 +0100

    Fix starting endpoint with secrets handling

--- images/docker-zwavejs/runwithsecrets.sh
+++ images/docker-zwavejs/runwithsecrets.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 
 # Read each line from the file and export it as an environment variable
 # see https://raw.githubusercontent.com/zwave-js/zwave-js-ui/master/docs/guide/env-vars.md
 # for available variables
 while IFS= read -r line; do
-  export "$line"
-done < /run/secrets/zwgate_env_secret
+  export $line
+done < /run/secrets/zwgate_secrets
 
-exec "$@"
+exec $@
