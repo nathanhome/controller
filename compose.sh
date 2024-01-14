8e67d3048fef3dfd28f117b771340fb86bade006
brederle <rbe-github@t-online.de>
2024-01-14 18:39:18 +0100

    Add zigbee zigate version

--- compose.sh
+++ compose.sh
@@ -4,7 +4,9 @@ NATHAN_MQTT_VERSION=2.0-2 \
 NATHAN_MARIADB_VERSION=11.2 \
 NATHAN_HASS_VERSION=2023.12-2 \
 NATHAN_ZWAVEJS_VERSION=9.6-2 \
+NATHAN_ZIGBEE2MQTT_VERSION=1.35.1 \
 NATHAN_VERSION=23.0.2 \
 NATHAN_GROUP=$(getent group homeauto  | cut -d: -f3) \
 DIALOUT_GROUP=$(getent group dialout | cut -d: -f3) \
+TTY_GROUP=$(getent group tty | cut -d: -f3) \
 docker-compose $@
